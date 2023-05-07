4e5ba6f9671a72d87ae8f51b0770e3d317aed972
Elad Ben-David <eladbe@checkpoint.com>
2023-05-07 12:21:30 +0300

    adding flash card practice V2

--- MathFlashCards/script.js
+++ MathFlashCards/script.js
@@ -1,21 +1,25 @@
 function validation(numOne,numTwo){
     var userAnswer = document.getElementById("answer").value;
+    var correctAnswer = numOne + numTwo;
     if(isNaN(userAnswer)){
-
         document.getElementById("output").innerHTML = userAnswer + " isn't a number!";
-
     }
-    
     else {
-        if (numOne + numTwo == userAnswer){
-
-            document.getElementById("output").innerHTML = userAnswer + " is the correct answer!";
+        if (correctAnswer == userAnswer){
+            document.getElementById("output").innerHTML = "Correct! " + numOne + " + " + numTwo + " = " + correctAnswer + " is the correct answer!";
             }
             else{
-    
-            document.getElementById("output").innerHTML = userAnswer + " isn't the correct answer!"
+            document.getElementById("output").innerHTML = "Incorrect! " + numOne + " + " + numTwo + " = " + correctAnswer + " not " + userAnswer + "!";
             }
     }
     
 
+}
+function reset(){
+
+    document.getElementById("output").innerHTML = ""
+    document.getElementById("answer").value = ""
+    numOne = Math.floor(Math.random() * 10 + 1);
+    numTwo = Math.floor(Math.random() * 10 + 1);
+    document.getElementById("data").innerHTML = numOne + " + " + numTwo;
 }
